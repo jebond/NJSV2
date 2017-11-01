e314b3504a9b5c1c82a1a866d92b3507de0e043b
jebond <jebond@trollandtoad.com>
2017-11-01 17:36:40 -0400

    dsgds
    dsfg

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var whois = require('node-xwhois');
 
 server.listen(80);
 console.log('Here we are again listening on port 80');
 
 app.get('/', function (req, res) {
+  ip = req.ip;
+      simpleip = ip.substr(7);
+      whois.reverse(simpleip)
+      .then(hostnames => hostname = hostnames);
   res.sendFile(__dirname + '/index.html');
+  res.cookie('TNTScale',hostname);
 });
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
+  var parse = cookie.parse(socket.handshake.headers.cookie);
+  socket.emit('hostinformation', { 
+  	hostname: parse });
   socket.on('my other event', function (data) {
     console.log(data);
   });
+  socket.on('add user',function(data){
+
+  })
 });
