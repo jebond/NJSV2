7a04d1015a0c0a1315e92a01b0a9d9ee919167ad
jebond <jebond@trollandtoad.com>
2017-11-02 13:08:35 -0400

    fsd
    sad

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var io = require('socket.io')(server);
 var whois = require('node-xwhois');
 var cookieParser = require('cookie-parser');
 var ioidhostname = [];
+var hostname = null;
 //app config
 app.use(cookieParser());
 //server init
@@ -12,14 +13,15 @@ server.listen(80);
 console.log('Here we are again listening on port 80');
 //routes
 app.get('/', function (req, res) {
-  while (hostname === undefined)
+  
+  while (hostname === null)
   {
   	  ip = req.ip;
       simpleip = ip.substr(7);
       whois.reverse(simpleip)
       .then(hostnames => hostname = hostnames);
   }
-  
+
   var sessionid = req.cookies;
   ioidhostname.push({"hostname": hostname,"sessionid":sessionid});
   res.sendFile(__dirname + '/index.html');
